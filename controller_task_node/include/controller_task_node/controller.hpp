f793017f361a2172c380a35ca1fa927a5b239cea
mustafasamed <68030580+mustafasamed@users.noreply.github.com>
2023-07-11 21:21:32 +0300

    Update controller.hpp
    lateral control has not been finished yet
    longitudinal control has not been started yet
    Rviz viewing was accomplished but has not been checked after lateral control changes.

--- controller_task_node/include/controller_task_node/controller.hpp
+++ controller_task_node/include/controller_task_node/controller.hpp
@@ -12,54 +12,186 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#ifndef CONTROLLER_TASK_NODE__CONTROLLER_HPP_
-#define CONTROLLER_TASK_NODE__CONTROLLER_HPP_
+ #include "controller_task_node/calculateDistance.hpp"
+ #include "controller_task_node/calculateSteeringAngle.hpp"
 
-#include <rclcpp/rclcpp.hpp>
+#include "controller_task_node/controller.hpp"
 
-#include <autoware_auto_control_msgs/msg/ackermann_control_command.hpp>
-#include <autoware_auto_planning_msgs/msg/trajectory.hpp>
-#include <autoware_auto_planning_msgs/msg/trajectory_point.hpp>
-#include <autoware_auto_vehicle_msgs/msg/gear_command.hpp>
-#include <geometry_msgs/msg/pose.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <std_msgs/msg/float64.hpp>
+#include <algorithm>
 
-#include <memory>
+// change begin //
+#include <fstream> // this is to include waypoints.txt file into controller.cpp. This makes a txt file is read or written by the controller.cpp.
+// change end //
+
 
 namespace controller
 {
-  using autoware_auto_control_msgs::msg::AckermannControlCommand;
-  using autoware_auto_planning_msgs::msg::Trajectory;
-  using autoware_auto_planning_msgs::msg::TrajectoryPoint;
-  using geometry_msgs::msg::Pose;
-  using geometry_msgs::msg::Twist;
-  using nav_msgs::msg::Odometry;
 
-  class Controller : public rclcpp::Node
+  Controller::Controller(const rclcpp::NodeOptions &options) 
+      : Node("controller", options),
+      visualization_rate_(50) // Initialize visualization_rate_ to 50
   {
-  public:
-    explicit Controller(const rclcpp::NodeOptions &options);
-    ~Controller() = default;
+        std::cout << "Controller constructor called!" << std::endl; // Debug output
+    // change begin //
+    // to define calculateDistance function and use it at OnTime()
 
-  private:
-    rclcpp::Subscription<Odometry>::SharedPtr sub_kinematics_;
-    rclcpp::Publisher<Trajectory>::SharedPtr pub_trajectory_;
-    rclcpp::Publisher<AckermannControlCommand>::SharedPtr pub_cmd_;
-    rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::GearCommand>::SharedPtr pub_gear_cmd_;
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_lateral_deviation_;
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_longitudinal_velocity_error_;
+    // Read trajectory points from the waypoints.txt file
+    std::ifstream file("/home/mustafasamed/leo_ws/src/control_task/controller_task_node/config/waypoints.txt");
+    if (file.is_open())
+    {
+        double x, y, z, qx, qy, qz, qw, velocity;
+        while (file >> x >> y >> z >> qx >> qy >> qz >> qw >> velocity)
+        {
+            // Create a TrajectoryPoint message and add it to the trajectoryPoints_ vector
+            TrajectoryPoint point;  // after "point.", pose and longitudinal_velocity_mps were added to match with the mesage structure.
+            point.pose.position.x = x;
+            point.pose.position.y = y;
+            point.pose.position.z = z;
+            point.pose.orientation.x = qx;
+            point.pose.orientation.y = qy;
+            point.pose.orientation.z = qz;
+            point.pose.orientation.w = qw;
+            point.longitudinal_velocity_mps = velocity; // this is longitudinal velocity(forward velocity).
+            trajectoryPoints_.push_back(point); // this line makes trajectoryPoints_ vector to store all the waypoint values which are stored in point variable of TrajectoryPoint type.
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout<<"waypoints.txt file cannot be read.";
+    }
+// change end //
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    Odometry::SharedPtr odometry_;
+    pub_cmd_ = create_publisher<AckermannControlCommand>("/control/command/control_cmd", 1);
+    pub_trajectory_ = create_publisher<Trajectory>("/control/trajectory", 1);;
+    pub_lateral_deviation_ = create_publisher<std_msgs::msg::Float64>("/control/lateral_deviation", 1);;
+    pub_longitudinal_velocity_error_ = create_publisher<std_msgs::msg::Float64>("/control/longitudinal_velocity_error", 1);;
+    pub_gear_cmd_ = create_publisher<autoware_auto_vehicle_msgs::msg::GearCommand>("/control/command/gear_cmd", 1);;
+    sub_kinematics_ = create_subscription<Odometry>(
+        "/localization/kinematic_state", 1, [this](const Odometry::SharedPtr msg)
+        { odometry_ = msg; });
 
-    void onTimer();
-    double calcSteerCmd();
-    double calcAccCmd();
-    double calcLateralDeviation();
-  };
+    using namespace std::literals::chrono_literals;
+    timer_ = rclcpp::create_timer(
+        this, get_clock(), 30ms, std::bind(&Controller::onTimer, this));
+  }
+
+    void Controller::findClosestTrajectoryPoint() // this function is to prevent having dublicate codes in both OnTime() & calcSteerCmd()
+  {
+      int num_points = trajectoryPoints_.size();
+      min_distance_ = std::numeric_limits<double>::max();
+      start_index_ = 0;
+      if (odometry_ && !trajectoryPoints_.empty())
+      {
+          //const auto& current_position = odometry_->pose.pose.position;
+          for (int i = 0; i < num_points; ++i)
+          {
+              const auto& point = trajectoryPoints_[i].pose.position;
+              double distance = calculateDistance(odometry_->pose.pose.position.x, odometry_->pose.pose.position.y, point.x, point.y);
+              if (distance < min_distance_)
+              {
+                  min_distance_ = distance;
+                  start_index_ = i;
+              }
+          }
+      }
+  }
+
+
+  void Controller::onTimer()
+  {
+    // To make vehicle able to drive, we need to publish the gear command to drive
+
+    autoware_auto_vehicle_msgs::msg::GearCommand gear_cmd;
+    gear_cmd.command = autoware_auto_vehicle_msgs::msg::GearCommand::DRIVE;
+    gear_cmd.stamp = this->now();
+    pub_gear_cmd_->publish(gear_cmd);
+
+    // change begin //
+    // Publish trajectory points for visualization in RViz
+    Trajectory trajectory; // this is a variable of Trajectory type.
+    trajectory.header.stamp = this->now(); 
+    trajectory.header.frame_id = "map";
+
+    // Calculate the start index based on the car's position
+    int num_points = trajectoryPoints_.size(); // to compare with an integer "i", we also need an integer.
+    
+    // Call the helper function to find the closest trajectory point
+    findClosestTrajectoryPoint();
+
+    // Determine the end index based on the start index and visualization rate
+    int end_index = std::min(start_index_ + visualization_rate_, num_points);
+
+    // Add the selected trajectory points to the Trajectory message
+    for (int i = start_index_; i < end_index; ++i)
+    {
+        trajectory.points.push_back(trajectoryPoints_[i]);
+    }
+    pub_trajectory_->publish(trajectory);
+  }
+    // change end //
+
+
+    // Calculate the control command and error values.
+  
+
+  double Controller::calcSteerCmd()
+  {
+    //double steer = 0.0;
+    // Calculate the steering angle here.
+    
+    // change begin //
+    
+    // Call the helper function to find the closest trajectory point
+    findClosestTrajectoryPoint(); // might be unnecessary
+
+    // Calculate the steering angle here.
+    double previous_lateral_error = 0/* obtain previous lateral error */;
+    double integral_of_lateral_error = 0/* obtain integral of lateral error */;
+
+
+    double steer = calculateSteeringAngle(lateral_deviation_, integral_of_lateral_error, previous_lateral_error);
+    // change end //
+
+    return steer;
+  }
+
+  double Controller::calcAccCmd()
+  {
+    double acc = 0.0;
+    // Calculate the acceleration here.
+    return acc;
+  }
+  double Controller::calcLateralDeviation() // I added this function into controller class since it is added in controller.hpp private section and I can use declared variables of controller class in this function.
+  {
+    double lateral_deviation = 0.0;
+    // Calculate the lateral deviation here.
+
+      // change begin //
+  // Check if the trajectoryPoints_ vector is not empty
+  if (!trajectoryPoints_.empty()) // this-> is added since calcLateralDeviation() is not a member func of controller clas
+  {
+    // Get the desired lateral position based on the start_index
+    double desired_lateral_position = trajectoryPoints_[start_index_].pose.position.y;
+
+    // Check if the odometry_ message is valid
+    if (odometry_)
+    {
+      // Get the current lateral position from the odometry message
+      double current_lateral_position = odometry_->pose.pose.position.y;
+
+      // Calculate the lateral deviation as the difference between current and desired lateral positions
+      lateral_deviation = current_lateral_position - desired_lateral_position;
+    }
+  }
+
+    lateral_deviation_ = lateral_deviation;
+    // change end //
+    std::cout<<"lateral_deviation:"<<lateral_deviation<<std::endl;
+    return lateral_deviation;
+  }
 
 } // namespace controller
 
-#endif // CONTROLLER_TASK_NODE__CONTROLLER_HPP_
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(controller::Controller)
